c891830dfab3dd6d64175e39898e40ac400c5e83
hardik354 <hardikjain.it25@gmail.com>
2024-10-27 09:13:29 +0530

    new 2

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
-mongoose.connect("mongodb://127.0.0.1:27017/SCATCH")
-.then(() => console.log("MongoDB Connected..."))
-.catch(err => console.log(err))
+const dbgr = require("debug")("development:mongoose-connection");
 
+mongoose.connect(`${config.get("MONGODB_URI")}/SCATCH`)
+.then(function () {
+     dbgr("MongoDB Connected...");
+})
+.catch(function (err) {
+     dbgr(err); 
+});
 module.exports = mongoose.connection;
+
+// $env:DEBUG="development:mongoose-connection"; node yourScript.js
+// debug and config are both packages for Node.js that provide debugging and configuration functionality.
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+
+// creating a owner and i want this will work only for development phase
+if (process.env.NODE_ENV === 'development'){
+    router.post('/create', async (req, res) => {
+        // res.send('hey it is working');
+     let owners =   await ownerModel.find();
+     if(owners.length > 0)
+     {
+        return res.status(503).send('Owner already exists');
+     }
+     let { fullname, email, password } = req.body;
+     let createdOwner =  await ownerModel.create({
+        fullname,
+        email,
+        password,
+        // products: [],
+        // gstin: req.body.gstin,
+        // picture: req.body.picture,
+     });
+     res.status(201).send(createdOwner);
+    });    
+}
+
 
 router.get('/', (req, res) => {
     res.send('This is working');
 });
 
+
 module.exports = router;
+// $env:NODE_ENV="production"; node yourScript.js
